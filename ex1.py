7dae2c89b0d246235b0a019832ace28d40b338d1
Blomman <christian.agren.1@gmail.com>
2016-09-09 09:12:10 +0200

    added question

--- ex1.py
+++ ex1.py
@@ -3,6 +3,24 @@ import sys
 import math
 import numpy as np
 
+"""
+Write a computer program imple-menting the Hopfield model with synchronous
+updating (LN1pp. 10 and 14)according to the McCulloch-Pitts rule
+
+Si <- sgn(sum_j(Wij Sj)).
+
+The weights wij are given by Hebb’s rule fori6=j, andwii= 0. Store p random
+patterns (LN pp. 16 and 19) and use your computer program tofind how the
+one-step error probabilityPErrordepends onp/N. Parameters:
+
+N= 100,200
+p= 10,20,30,40,50,75,100,150,200
+
+For each datapoint average over enough independent trials so that you obtain
+a precise estimate of PError. Plot your simulation results together with the
+correspondingtheoretical curve as a function of p/N. Discuss
+"""
+
 def read_data(path):
     f = open(path)
     data = []
