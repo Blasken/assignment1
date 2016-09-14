6607e2f078e599d59331702099573024edbc8939
Benjamin Lindberg <benjamin.lindberg@gmail.com>
2016-09-14 14:17:32 +0200

    Think i am done with ex3

--- ex3.py
+++ ex3.py
@@ -21,27 +21,33 @@ same range as above. Discuss how the value of N influences the order parameter m
 
 
 def run():
-    N = 500
-    alphas = np.linspace(0.01,0.2,20)
-    mValues = np.zeros(alphas.size)
-    iters = 1000
-    nRealisations = 10
-    for k in range(alphas.size):
-        a = alphas[k]
-        mSum = 0
-        for _ in range(nRealisations):
-            patterns = common.random_patterns(N,int(a*N))
-            W = common.hebbs_rule(patterns)
-            ms = np.zeros(iters)
-            S = patterns[0]
-            for i in range(iters):
-                ms[i] = S.dot(patterns[0])/N
-                S = nextState(S,W,2)
-            mSum += np.average(ms[100:])
-        mValues[k] = mSum/nRealisations
-    plt.plot(alphas,mValues)
-    plt.show()
-    #plt.close()
+    Ns = [50, 100, 250, 500]
+    iters = 400
+    nRealisations = 50
+    plt.clf()
+    for N in Ns:
+        ps = np.array([int(i) for i in np.linspace(1,N/2,N/2)])
+        print(ps.size)
+        alphas = ps/N
+        mValues = np.zeros(alphas.size)
+        for k in range(alphas.size):
+            p = ps[k]
+            mSum = 0
+            for _ in range(nRealisations):
+                patterns = common.random_patterns(N,p)
+                W = common.hebbs_rule(patterns)
+                ms = np.zeros(iters)
+                S = patterns[0]
+                for i in range(iters):
+                    ms[i] = S.dot(patterns[0])/N
+                    S = nextState(S,W,2)
+                mSum += np.average(ms[100:])
+            mValues[k] = mSum/nRealisations
+        plt.plot(alphas,mValues,label="N = {}".format(N))
+    plt.ylabel(r'Order parameter, $m_1$',fontsize = 16)
+    plt.legend()
+    plt.xlabel(r'$\alpha = p/N$',fontsize = 16)
+    plt.savefig('ex3a')
 
 def nextState(S,W,beta=2):
     return np.sign((np.random.rand(S.size) < 1/(1 + np.exp(-2*beta*W.dot(S))))-0.5)
