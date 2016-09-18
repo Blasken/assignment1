afbb5d4dfa14022cc11feba26a86ec48dead08d6
Blasken <christian.agren.1@gmail.com>
2016-09-18 13:04:23 +0200

    Fixed plot

--- ex1.py
+++ ex1.py
@@ -29,8 +29,6 @@ def run(steps=10):
     P = [10, 20, 30, 40, 50, 75, 100, 150, 200]
     errors = []
 
-#     Ns = [200]
-#     P = [10]
     for N in Ns:
         sub_errors = []
         for p in P:
@@ -38,12 +36,10 @@ def run(steps=10):
             for _ in range(steps):
                 patterns = common.random_patterns(N, p)
                 W = common.hebbs_rule(patterns)
-                subsum += np.sum(np.not_equal(patterns, step(W,patterns))) / (p*N)
+                subsum += np.sum(np.not_equal(patterns, step(W, patterns))) / (p*N)
             sub_errors.append(subsum / steps)
-#             print(erfc(np.sqrt(N/(2*p)))/2)
         errors.append(sub_errors)
-    print(errors)
-    _plot(errors,P)
+    _plot(errors, N, P)
 
 def step(W, S):
     return np.sign(W.dot(S.T)).T
@@ -51,23 +47,23 @@ def step(W, S):
 def p_error(S, S_next):
     pass
 
-def _plot(errors,P):
-    X = np.linspace(0,1)
-    y = np.sqrt(X)
-    Y = erfc(y)/2
-#     print(erfc(np.sqrt(N/(2*p)))/2)
-    plt.plot(X,Y)
+def _plot(errors, N, P):
     x1 = [p / 100 for p in P]
     x2 = [p / 200 for p in P]
-#     plt.scatter(x1,errors[0])
-#     plt.scatter(x2,errors[1])
-    plt.scatter(x2,errors[0])
-#     plt.scatter(x2,errors[1])
+    p = np.linspace(min(P)/2,max(P))
+    Y = erfc(np.sqrt(100/(2*p)))/2
+    erf = plt.plot(p/100, Y, 'k-', label="Theoretical")
+    n100 = plt.plot(x1, errors[0], color='b', marker='+', linestyle='', label="N=100")
+    n200 = plt.plot(x2, errors[1], color='r', marker='x', linestyle='', label="N=200")
 
+    plt.legend()
+#     plt.legend((n100, n200), ("N=100", "N=200"), scatterpoints=1)
+    plt.xlabel("p/N")
+    plt.ylabel("P(Error)")
     plt.show()
 
 def crosstalk_term(patterns):
-    dsum = patterns.dot(patterns.T)
+    dsum = patterns.dot(patterns.t)
     np.fill_diagonal(dsum, 0)
     dsum = patterns.dot(dsum)
     dsum = dsum / len(patterns[0])
