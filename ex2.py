19d320e500fc0ad31d15307ccc07f2d3b5b1d748
Blomman <christian.agren.1@gmail.com>
2016-09-19 16:26:54 +0200

    doing things right

--- ex2.py
+++ ex2.py
@@ -1,5 +1,7 @@
 import numpy as np
 import common
+import matplotlib.pyplot as plt
+from common import random_patterns as rp
 
 """
 Recognising digits with the deterministic Hopfield model.Thetask is to
@@ -11,19 +13,48 @@ Feed these patterns to the network and use asynchronous updating. For each
 digit, repeat many times. Determine and plot the probability that the
 network retrieves the correct pattern as a function of q. Discuss.
 """
-def run():
+def run(steps=100):
     digits = common.digits()
     W = common.hebbs_rule(digits)
 
 #     for q in range(0.1, 0.9, 0.001):
 #         p = random_patterns(len(digits[0]), len(digits), q)
 
-    distorted = digits * common.random_patterns(len(digits[0]), len(digits), 0.3)
-    print(distorted)
-    print(np.sum(np.not_equal(distorted, digits)))
+#     distorted = digits * rp(len(digits[0]), len(digits), 0.3)
 
-    for digit in distorted:
-        astep(W,digit)
+    distorted_list = [digits * rp(len(digits[0]), len(digits), q)
+                                 for q in np.linspace(0.1,0.9,9)]
 
-def astep(W, digit):
-    pass
+    ilist = [i for i in range(len(W[0]))]
+
+    for q in np.linspace(0.1, 0.5, 10):
+        errors = []
+        error = 0
+        print("q =", q)
+        for n in range(steps):
+            distorted = digits * rp(len(digits[0]), len(digits), q)
+#             print(np.sum(np.not_equal(distorted, digits)))
+            distorted = astep(W, distorted)
+#         np.random.shuffle(ilist)
+#         for digit in distorted:
+#             for i in ilist:
+#                 digit[i] = astep(W[i],digit)
+#                 if not i % 40:
+#                 plt.imshow(digit.reshape((16,10)))
+#                 plt.show()
+#             plt.imshow(digit.reshape((16,10)))
+#             plt.show()
+#             print(np.sum(np.not_equal(digits, distorted)))
+            error += (np.sum(np.not_equal(digits, distorted))) / steps
+        errors.append(error / digits.size)
+        print(error / digits.size)
+    print(errors)
+    print()
+
+def astep(W, digits):
+    ilist = [i for i in range(len(W[0]))]
+    np.random.shuffle(ilist)
+    for digit in digits:
+        for i in ilist:
+            digit[i] = np.sign(W[i].T.dot(digit))
+    return digits
