9425827b524b473b7b6989e6e2920097e70d351a
Blomman <christian.agren.1@gmail.com>
2016-09-24 13:42:07 +0200

    featuring: solution now in the cloud

--- ex2.py
+++ ex2.py
@@ -14,39 +14,53 @@ Feed these patterns to the network and use asynchronous updating. For each
 digit, repeat many times. Determine and plot the probability that the
 network retrieves the correct pattern as a function of q. Discuss.
 """
-def run(iters=100):
+def run(iters=100, debug=False):
     digits = common.digits()
     W = common.hebbs_rule(digits)
+    P, N = digits.shape
 
     x_axis = [q/100 for q in range(100)]
-    errors = []
+    error_mean = []
+    error_digit = []
     for q in x_axis:
-        error = 0
-#         print("q =", q)
+        error_d = np.zeros(P)
         for n in range(iters):
-            distorted = digits * rp(len(digits[0]), len(digits), q)
-            distorted = astep(W, distorted)
-#         np.random.shuffle(ilist)
-#         for digit in distorted:
-#             for i in ilist:
-#                 digit[i] = astep(W[i],digit)
-#                 if not i % 40:
-#                 plt.imshow(digit.reshape((16,10)))
-#                 plt.show()
-#             plt.imshow(digit.reshape((16,10)))
-#             plt.show()
-#             print(np.sum(np.not_equal(digits, distorted)))
-            error += np.sum(np.not_equal(digits, distorted))
-        errors.append(error / (iters * digits.size))
-    plt.plot(x_axis,errors)
-    y = expit(np.linspace(-6,6,100))
-    plt.plot(x_axis, y)
-    plt.show()
+            distorteds = digits * rp(N, P, q)
+            for i, (digit, distorted) in enumerate(zip(digits, distorteds)):
+                lucky = True
+                updates = 1
+                while lucky and not np.array_equal(digit, distorted):
+                    temp_test = np.copy(distorted)
+                    distorted = astep(W, distorted)
+                    lucky = updates % 5 and not np.array_equal(temp_test, distorted)
+                    if debug and lucky and not updates % 3:
+                        print("Wrong updates were made, still feeling lucky for"
+                                " digit ", i, ". Try #", updates, " now...")
+                    updates += 1
+                error_d[i] += np.sum(np.not_equal(digit, distorted))
+                if debug:
+                    if not lucky and error_d[i]:
+                        err = error_d[i] / ((n+1) * N)
+                        print("Was not lucky, no progress after ", updates, " tries"
+                                ". Stuck on error ", err, " for digit ", i)
+                    if not error_d[i]:
+                        print("perfect run! q=", q, " digit ", i, " after ", updates)
+        error_digit.append(1 - error_d / (iters * N))
+        error_mean.append(np.sum(error_digit[-1]) / P)
+    plt.plot(x_axis, error_digit)
+    plt.plot(x_axis, error_mean)
+    plt.legend(["0", "1", "2", "3", "4", "mean"])
+    plt.xlabel("P(pattern | q)")
+    plt.ylabel("Correct pattern")
+#     y = expit(np.linspace(-6,6,100))
+#     plt.plot(x_axis, y)
+    plt.savefig("ex2")
+#     plt.show()
 
-def astep(W, digits):
+def astep(W, digit):
     ilist = [i for i in range(len(W[0]))]
     np.random.shuffle(ilist)
-    for digit in digits:
-        for i in ilist:
-            digit[i] = np.sign(W[i].T.dot(digit))
-    return digits
+#     for digit in digits:
+    for i in ilist:
+        digit[i] = np.sign(W[i].T.dot(digit))
+    return digit
