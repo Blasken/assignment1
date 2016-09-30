36c292f4eec65650bf4e2742231f083f9f53eae5
Blomman <christian.agren.1@gmail.com>
2016-09-30 10:13:44 +0200

    Removed problem descriptions

--- ex2.py
+++ ex2.py
@@ -4,16 +4,6 @@ import common
 import matplotlib.pyplot as plt
 from common import random_patterns as rp
 
-"""
-Recognising digits with the deterministic Hopfield model.Thetask is to
-recognise distorted versions of the digits shown in Fig. 1 using the
-deterministic Hopfield model with asynchronous updating.  Create binary
-representations of the digits shown in Fig. 1 and store these patterns.
-Then distort the patterns by flipping a fraction q of randomly chosen bits.
-Feed these patterns to the network and use asynchronous updating. For each
-digit, repeat many times. Determine and plot the probability that the
-network retrieves the correct pattern as a function of q. Discuss.
-"""
 def run(iters=100, show_updates=False):
     digits = common.digits()
     W = common.hebbs_rule(digits)
--- ex3.py
+++ ex3.py
@@ -2,22 +2,6 @@ import common
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-Stochastic Hopfield model. Write a computer program implementing the Hopfield
-model with stochastic updating (LN pp. 36). Use Hebb’s rule and take w ii = 0.
-Use random patterns (LN pp. 16 and 19).
-
-a) For β = 2 determine the steady-state order parameter m 1 as a function of
-α = p/N for N = 500. Discard data points corresponding to the initial transient
-of the stochastic dynamics. Average over independent realisations. Plot the
-order parameter as a function of α in the range 0 < α ≤ 1. Discuss your results.
-Refer to the phase diagram (LN p. 60).( 1p ).
-
-b) Repeat the above for N = 50 , 100 , and 250 choosing p so that α is in the
-same range as above. Discuss how the value of N influences the order parameter m1 .
-"""
-
-
 def run():
     Ns = [50, 100, 250, 500]
     iters = 300
